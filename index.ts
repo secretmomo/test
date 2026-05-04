6cf00dde798fa9a2ca837e3bdcffbd72df87d21d
secretmomo <secret_momo@protonmail.com>
2026-05-04 21:35:04 +0800

    chore

--- index.ts
+++ index.ts
@@ -1,20 +1,6 @@
 import dayjs from "dayjs";
-import utc from "dayjs/plugin/utc";
-import timezone from "dayjs/plugin/timezone";
 
 console.log("start");
 console.log(dayjs().format("YYYY-MM-DD HH:mm:ss"));
 
-dayjs.extend(utc);
-dayjs.extend(timezone);
-
-console.log("before set default");
-console.log(dayjs().format("YYYY-MM-DD HH:mm:ss"));
-console.log(dayjs().tz().format("YYYY-MM-DD HH:mm:ss"));
-
-/** 北京时间（中国标准时间，IANA: Asia/Shanghai） */
-dayjs.tz.setDefault("Asia/Shanghai");
-
-console.log("after set default");
-console.log(dayjs().format("YYYY-MM-DD HH:mm:ss"));
-console.log(dayjs().tz().format("YYYY-MM-DD HH:mm:ss"));
+console.log("end");
