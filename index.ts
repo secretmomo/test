a9fe30d736937b150158fd04760a835602b488e8
secretmomo <secret_momo@protonmail.com>
2026-05-05 16:36:25 +0800

    x

--- index.ts
+++ index.ts
@@ -4,3 +4,9 @@ console.log("start");
 console.log(dayjs().format("YYYY-MM-DD HH:mm:ss"));
 
 console.log("end");
+
+
+const url = `https://www.szse.cn/api/report/exchange/onepersistenthour/monthList?month=${dayjs().format("YYYY-MM")}`;
+const res = await fetch(url) ;
+const data = await res.json();
+console.log(data);
